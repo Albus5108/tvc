80c7c773ac7bcee7c932190d19d227bf4dc6254b
Albus5108 <arthur.petit5108@gmail.com>
2026-01-28 22:55:10 +0100

    add globals.R
    add globals.R

---
+++ R/globals.R
@@ -0,0 +1,24 @@
+globalVariables(unique(c(
+  # clean_song_title:
+  "Artist", "Song",
+  # get_playlist_id:
+  "playlist", "triad_identifier", "triad_name",
+  # get_spotify_api_playlist:
+  "Album_Date", "Album_Year", "Artist", "artist_name", "data", "genres", "id", "is_local", "ISRC", "items", "Playlist_Order", "primary_color", "Song", "track.album.album_type", "track.album.artists", "track.album.id", "track.album.release_date", "track.album.type", "track.artists", "track.episode", "track.external_ids.isrc", "track.name", "track.preview_url", "track.track", "track.type",
+  # get_spotify_api_triad_playlists:
+  "name", "playlist", "triad",
+  # parse_triade:
+  "Affirmation", "Artist", "Degree", "Harmonie Sous-Jacente", "ISRC", "Key", "Mode", "name", "playlist", "Song", "Tmp", "Tmp_Majeur", "Tmp_Mineur", "triad",
+  # plot_tvc_histogram:
+  "Album_Year", "ISRC", "Song",
+  # plot_tvc_network:
+  ".", "Affirmation", "Artist", "conf", "Degree", "from", "Genre", "genres", "Genres", "Group", "Harmonie Sous-Jacente", "id", "Instrument", "ISRC", "ISRC_from", "ISRC_to", "Mode", "Nb_Chansons", "Ordre des Notes", "size", "Song", "to", "track.popularity", "Type", "x", "xend", "y", "yend",
+  # plot_tvc_world:
+  "Artist", "nb_triad", "Ordre des Notes", "region", "Region", "Region_Group",
+  # triad_coverage:
+  "First_Interval", "ISRC", "name", "Nb_Songs", "Ordre des Notes", "playlist", "Second_Interval", "Song", "triad", "triad_tmp",
+  # write_missing_region:
+  "Artist", "genres",
+  # write_missing_triade:
+  "Artist", "ISRC", "Ordre des Notes", "Playlist_Order", "Song", "triad"
+)))
